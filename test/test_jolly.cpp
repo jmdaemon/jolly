8fbe00c114d6b3824f0a222e394844acb19d9957
Joseph Diza <josephm.diza@gmail.com>
2022-08-24 00:18:37 -0700

    Fix failing parse_datetime() test

--- test/test_jolly.cpp
+++ test/test_jolly.cpp
@@ -3,17 +3,6 @@
 #include "jolly.h"
 #include <fmt/core.h>
 
-void show_tm(std::tm tm) {
-    fmt::print("Time Struct:");
-    fmt::print("tm.tm_year : {}\n", tm.tm_year); // Date
-    fmt::print("tm.tm_mon  : {}\n", tm.tm_mon);
-    fmt::print("tm.tm_mday : {}\n", tm.tm_mday);
-
-    fmt::print("tm.tm_hour : {}\n", tm.tm_hour); // Time
-    fmt::print("tm.tm_min  : {}\n", tm.tm_min);
-    fmt::print("tm.tm_sec  : {}\n", tm.tm_sec);
-}
-
 TEST_CASE("trim() removes whitespace from strings") { 
   std::string s1 = " left";
   std::string s2 = "right ";
@@ -34,12 +23,6 @@ TEST_CASE("substitute() replaces substrings in string") {
   CHECK(substitute(s, "replace", "new") == "[new]");
 }
 
-TEST_CASE("prompt() substrings in string") { 
-  //prompt("Enter any integer: ");
-  //std::cout << 5 << std::endl;
-
-}
-
 TEST_CASE("parse_datetime() parses datetimes") {
   std::string datetime = "2021-07-14T02:10:41Z";
   std::string format;
@@ -49,11 +32,9 @@ TEST_CASE("parse_datetime() parses datetimes") {
     fmt::print("datetime  : {}\n", datetime);
     fmt::print("format    : {}\n", format);
     auto tm = parse_datetime(datetime, format);
-    show_tm(tm);
 
     // Checks
-    CHECK(tm.tm_year == 2001);
-    CHECK(tm.tm_yday == 07);
-    CHECK(tm.tm_yday == 07);
+    CHECK(mktime(&tm) != 0);
+    CHECK(asctime(&tm) != NULL);
   }
 }
