45956e6e455372d16d3ece39b4b5a62c1eaae1f2
Joseph Diza <josephm.diza@gmail.com>
2022-08-23 22:30:46 -0700

    Add tests for trim

--- test/test_jolly.cpp
+++ test/test_jolly.cpp
@@ -2,6 +2,12 @@
 #include "doctest.h"
 #include "jolly.h"
 
-TEST_CASE("Test") { 
-  REQUIRE(1 == 2);
+TEST_CASE("Trim removes whitespace from strings") { 
+  std::string s1 = " left";
+  std::string s2 = "right ";
+  std::string s3 = " both ";
+
+  REQUIRE(ltrim(s1) == "left");
+  REQUIRE(rtrim(s2) == "right");
+  REQUIRE(trim(s3) == "both");
 }
