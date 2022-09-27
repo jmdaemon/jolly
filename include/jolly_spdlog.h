fe4a129afb9cd8853c8819aa9b588d6b15b27c90
Joseph Diza <josephm.diza@gmail.com>
2022-09-27 00:49:24 -0700

    Add additional macro to init spdlog for applications

--- include/jolly_spdlog.h
+++ include/jolly_spdlog.h
@@ -7,6 +7,19 @@
   * https://stackoverflow.com/questions/30400286/logging-from-library-with-spdlog
   * https://github.com/gabime/spdlog/issues/1224 */
 
+// Functions
+//void disable_spdlog_macro_logging();
+//void init_spdlog_dynamic_logging(int verbose);
+
+/** Disables spdlog macro logging statements
+  * Note that this function is used internally and you should not really be using this. */
+void disable_spdlog_macro_logging() {
+  #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_NONE 
+}
+
+// Macros
+
+/** Registers new loggers and creates them if a logger hasn't been made */
 #define USE_JOLLY_SPDLOG_SETUP_LOGGER_FUNCTION \
 std::shared_ptr<spdlog::logger> setup_logger(std::vector<spdlog::sink_ptr> sinks) { \
   auto logger = spdlog::get(logger_name); \
@@ -21,39 +34,29 @@ std::shared_ptr<spdlog::logger> setup_logger(std::vector<spdlog::sink_ptr> sinks
   return logger; \
 }
 
-//#define JOLLY_DISABLE_SPDLOG_LOGGING(log_level) \
-  //log_level SPDLOG_LEVEL_NONE
-
-/** Enables or disables macro logging with spdlog library
-  * Note that if spdlog is not installed this turns into a no-op */
-//void toggle_spdlog_macro_logging(bool enable) {
-  ////if (!enable) {
-//#define SPDLOG_ENABLE enable
-//#if !enable
-  //#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_NONE 
-  ////} else {
-//#else 
-  //#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
-  //}
-//}
-
-void disable_spdlog_macro_logging() {
-  #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_NONE 
-}
-
+/* Enable/disable logging */
 #define USE_JOLLY_SPDLOG_SETUP_LOGGING_FUNCTION \
-/* Enable/disable logging */ \
 void setup_logging(bool verbose) { \
   if (verbose == 1) \
     spdlog::set_level(spdlog::level::level_enum::trace); \
   else { \
     /* Default to no logging */ \
     spdlog::set_level(spdlog::level::level_enum::off); \
-    /* toggle_spdlog_macro_logging(false); \ */ \
     disable_spdlog_macro_logging(); \
   } \
   spdlog::cfg::load_env_levels(); \
   spdlog::set_pattern(logger_format); \
+}
+
+/* Initializes the spdlog logger with a dynamic toggle */
+
+#define USE_JOLLY_SPDLOG_INIT_SPDLOG_DYNAMIC_LOGGING_FUNCTION \
+void init_spdlog_dynamic_logging(int verbose) { \
+  std::vector<spdlog::sink_ptr> sinks; \
+  sinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_st>()); \
+  auto logger = setup_logger(sinks); \
+  /*spdlog::set_default_logger(logger); */ \
+  setup_logging(verbose); \
 }
 
 #endif // JOLLY_SPDLOG_H
